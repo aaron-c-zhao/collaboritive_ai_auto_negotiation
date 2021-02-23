c2e24f8a0c1b8320c04f538e175a999c9b433c61
aaronzhao <C.Zhao-1@student.tudelft.nl>
2021-02-23 10:30:54 +0100

    Create the formula for agent's target utility
    The bidding strategy behaves differently both on time and opponent's niceness. The tougher the opponent is, the faster it concedes. On the other hand, it will try to exploit the nice opponent.

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 * SAOP: each agent can only respond to the most recent offer in its own turn. The turn taking sequences are predefined.
 
+## Description of components
+
+### Bidding Strategy
+
+f(t)= 1 - (t - 0.1)^a with a = 4
+![time function](./images/time_function_graph.png)
+
+
+
+
 ## FAQ
 
 ### How to debug the party in Intellij?
@@ -31,3 +41,16 @@ corresponding interfaces BiddingStrategy, AcceptanceStrategy and OpponentModel.
 3. return the strategy objects in the methods getOpponentModel, getBiddingStrategy,
    getAccceptanceStrategy in the Group42Party class.
    
+
+### How does the SOAP works on the object level?
+
+1. First party's lastbid == null, so she will make an offer by send an Offer obejct which is a derived
+class of Action.
+2. The Action is sent via ConnectionEnd's send() method. ConnectionEnd is an interface which is inherited
+by ProtocolToPartyConn interface.
+3. The SAOPState class, of which is a field of SOAP object, contains a list of ProtocolToPartyConns for each of the parties.
+4. The Action send in step 2 is wrapped in an ActionDone object and broadcast to all ProtocolToPartyConns by SOAP object.
+5. Both parties at this time receive an ActionDone info. 
+6. SOAP then will determine whether this is the final round. If not, it calls nextTurn and send a YourTurn info to 
+the corresponding party.
+7. The party who receives the YourTurn info, make her next action then. 
--- src/main/java/collabai/group42/biddingStrategy/Group42BiddingStrategy.java
+++ src/main/java/collabai/group42/biddingStrategy/Group42BiddingStrategy.java
@@ -2,7 +2,6 @@ package collabai.group42.biddingStrategy;
 
 import geniusweb.actions.Accept;
 import geniusweb.actions.Action;
-import geniusweb.actions.EndNegotiation;
 import geniusweb.actions.Offer;
 import geniusweb.actions.PartyId;
 import geniusweb.boa.BoaState;
@@ -11,9 +10,14 @@ import geniusweb.boa.biddingstrategy.ExtendedUtilSpace;
 import geniusweb.issuevalue.Bid;
 import geniusweb.profile.Profile;
 import geniusweb.profile.utilityspace.LinearAdditive;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
 import tudelft.utilities.immutablelist.ImmutableList;
 
+import java.lang.Math;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
@@ -23,6 +27,10 @@ public class Group42BiddingStrategy implements BiddingStrategy{
 
     private ExtendedUtilSpace bidSpace = null;
     private PartyId me;
+    private double a = 3.0, b = 0.15;
+    private final int maxToughness = 9, minToughness = 4;
+    private double prevUtility = 1.0;
+    private LinkedList<List<Double>> recentBids = new LinkedList<List<Double>>();
 
     @Override
     public Action getAction(BoaState boaState) {
@@ -33,17 +41,27 @@ public class Group42BiddingStrategy implements BiddingStrategy{
         double targetUtility = getTargetUtility(
                 boaState.getProgress().get(System.currentTimeMillis()));
 
+        Bid lastBid = getLastBid(boaState.getActionHistory());
+        if (!(lastBid == null) && recentBids.size() < 10)
+            recentBids.add(new ArrayList<>(Arrays.asList(prevUtility, getUtility(lastBid))));
+        else if (recentBids.size() == 10) {
+            recentBids.remove();
+            recentBids.add(Arrays.asList(prevUtility, getUtility(lastBid))));
+        }
+
+        prevUtility = targetUtility;
+
         ImmutableList<Bid> bidOptions = bidSpace
                 .getBids((BigDecimal.valueOf(targetUtility)));
 
-        // TODO: check the logic here
+        // TODO: Use opponentmodel to find the most beneficial bid wrt the opponent in the same
+        //       utility level
         if (bidOptions.size().intValue() == 0) {
             // should not happen, emergency exit
             boaState.getReporter().log(Level.WARNING,
                     "No viable bids found around current utility target");
-            Bid lastBid = getLastBid(boaState.getActionHistory());
-            if (lastBid == null)
-                return new EndNegotiation(me);
+            // TODO: replace this behavior with randomizing the utility
+
             return new Accept(me, lastBid);
         }
         Bid pickedBid = bidOptions.get(ThreadLocalRandom.current()
@@ -52,13 +70,116 @@ public class Group42BiddingStrategy implements BiddingStrategy{
     }
 
     /**
+     * Calculate the utility of certain bid wrt the party itself.
+     *
+     * @param lastBid the opponent's last bid.
+     * @return This party's utility gained with opponent's bid.
+     * TODO: implement this method
+     */
+    private double getUtility(Bid lastBid) {
+        return 0.0;
+    }
+
+    /**
      * The method where the specific bidding strategy is implemented.
+     * <p>
+     * The strategy combines time dependent pattern with behavior dependent pattern. Time decides
+     * the lower limit of the demanded utility, and the opponent's selfishness decides the upper limit
+     * of the demanded utility. The target utility will be drawn from the range randomly.
+     * <p>
+     * Once the target utility is decided, the opponent model is used to pick nicer bid wrt the opponent.
      *
      * @param progress Current progress wrt the total rounds
      * @return The intended utility for the next bid
      */
     private double getTargetUtility(Double progress) {
+        if (progress < 0.1) {
+            return 1 - progress;
+        }
+        //in the middle part of the negotiation, the toughness of the opponent affect the inflection point
+        //which is when to concede at a faster rate
+        else if (progress < 0.6) {
+            setB();
+            return getTimeDependUtility(progress);
+        }
+        //in the later half of the negotiation, the toughness of the opponent affect the concede ratio.
+        else if (progress < 0.9986){
+            setA();
+            return getTimeDependUtility(progress);
+        }
+        //only one round left, set the target utility to reservation value.
+        else {
+            return getMin();
+        }
+    }
+
+    /**
+     * Get the minimum utility. Default to reservation value(if present). If there's no
+     * reservation value, 0 is returned.
+     *
+     * @return The minimum acceptable utility value.
+     * TODO: implement this method
+     */
+    private double getMin() {
         return 0.0;
+    }
+
+    /**
+     * Formula for time dependent behavior.
+     *
+     * U(t) = 0.9 - (t - b)^a
+     *
+     * <li>b: [0.05 - 0.25]</li>
+     * <li>a: [2 - 5][</li>
+     *
+     * @param progress
+     * @return
+     */
+    private double getTimeDependUtility(Double progress) {
+        return 0.9 - Math.pow((progress - this.b), this.a);
+    }
+
+    /**
+     * Set the coefficient b which affects the conceding rate in phase 2.
+     */
+    private void setB() {
+        double niceness = getNiceness();
+        this.b =  0.05 + 0.2 * niceness;
+    }
+
+    /**
+     * Set the coefficient a which affects the conceding rate in phase 3.
+     */
+    protected void setA() {
+        double niceness = getNiceness();
+        this.a = 2 + 3 * niceness;
+    }
+
+    /**
+     * Calculate the opponent's niceness based on the slop of linear regression of the previous bids.
+     * <p>
+     *     If the slope is smaller than PI/maxToughness then the opponent is playing tough. Correspondingly,
+     *     the agent may consider to play extra tough to punish the opponent and end up breaking the deal.
+     * </p>
+     * <p>
+     *     If the slope is greater than PI/minToughness then the opponent is conceding normally,
+     *     then the nicer it plays the tougher the agent becomes. Vice versa.
+     * </p>
+     *
+     * @return the niceness of opponents which should be normalized into a range of [0, 1]
+     */
+    private double getNiceness() {
+        SimpleRegression regression = new SimpleRegression();
+        for (List<Double> point: recentBids) {
+            regression.addData(point.get(0), point.get(1));
+        }
+        double slope = regression.getSlope();
+
+        // map the slope into range [0, 1]
+        slope = (slope < Math.PI / maxToughness)? 0.0 : slope;
+        slope = (slope > Math.PI / minToughness)? 1.0 : slope;
+        slope = (slope - minToughness) / (maxToughness - minToughness);
+        return slope;
     }
 
     private void init(BoaState boaState) {
@@ -84,6 +205,7 @@ public class Group42BiddingStrategy implements BiddingStrategy{
      * Wrapper method. Enable testing bidding strategy with various opponent behavior.
      *
      * @return Estimated utility value of last bid from opponent model.
+     * TODO: call the actual API from opponent model
      */
     private double getOpponentUtility() {
         return 0.0;
