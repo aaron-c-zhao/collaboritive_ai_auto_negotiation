81463ca11b8118c67fb8ae08aef069f7bedfd1e9
aaronzhao <C.Zhao-1@student.tudelft.nl>
2021-03-01 16:03:11 +0100

    Fix the style of the code

--- src/main/java/collabai/group42/BoaState.java
+++ src/main/java/collabai/group42/BoaState.java
@@ -26,223 +26,216 @@ import java.util.Map;
 /**
  * Stores the current state of the negotiation for a {@link BoaParty}, including
  * the state of the opponent models. immutable.
- *
  */
 public class BoaState {
-	private final Settings settings;
-	private final Profile profile;
-	private final Progress progress;
-	private final Class<? extends OpponentModel> opponentModelClass;
-	private final Map<PartyId, OpponentModel> opponentModels;
-	private final List<Action> actionHistory;
-	private final BiddingStrategy biddingStrategy;
-	private final AcceptanceStrategy acceptanceStrategy;
-	private final Reporter reporter;
+    private final Settings settings;
+    private final Profile profile;
+    private final Progress progress;
+    private final Class<? extends OpponentModel> opponentModelClass;
+    private final Map<PartyId, OpponentModel> opponentModels;
+    private final List<Action> actionHistory;
+    private final BiddingStrategy biddingStrategy;
+    private final AcceptanceStrategy acceptanceStrategy;
+    private final Reporter reporter;
 
-	/**
-	 * Initial state. Update using with(settings).
-	 * 
-	 * @param reporter the {@link Reporter} to use for logging
-	 */
-	public BoaState(Reporter reporter) {
-		this(null, null, null, null, null, null, Collections.emptyList(),
-				Collections.emptyMap(), reporter);
-		if (reporter == null) {
-			throw new NullPointerException("reporter must be not null");
-		}
-	}
+    /**
+     * Initial state. Update using with(settings).
+     *
+     * @param reporter the {@link Reporter} to use for logging
+     */
+    public BoaState(Reporter reporter) {
+        this(null, null, null, null, null, null, Collections.emptyList(),
+                Collections.emptyMap(), reporter);
+        if (reporter == null) {
+            throw new NullPointerException("reporter must be not null");
+        }
+    }
 
-	/**
-	 * Internal constructor. Use
-	 *
-	 * @param settings      the negoi {@link Settings}
-	 * @param profile       the {@link Profile} to be used
-	 * @param progr         the {@link Progress}
-	 * @param bidstrat      the {@link BiddingStrategy}
-	 * @param acceptstrat   the {@link AcceptanceStrategy}
-	 * @param omClass       the class of the {@link OpponentModel}
-	 * @param actionHistory list of {@link Action}s that have been done in the
-	 *                      negotiatino (by us and others), first action is the
-	 *                      oldest.
-	 * @param oppMods       a map of {@link OpponentModel}s, one for each
-	 *                      {@link PartyId} encountered in the negotiation
-	 * @param reporter      the {@link Reporter} that can be used to log
-	 *                      messages. Should equal {@link BoaParty}'s reporter
-	 */
-	private BoaState(Settings settings, Profile profile, Progress progr,
+    /**
+     * Internal constructor. Use
+     *
+     * @param settings      the negoi {@link Settings}
+     * @param profile       the {@link Profile} to be used
+     * @param progr         the {@link Progress}
+     * @param bidstrat      the {@link BiddingStrategy}
+     * @param acceptstrat   the {@link AcceptanceStrategy}
+     * @param omClass       the class of the {@link OpponentModel}
+     * @param actionHistory list of {@link Action}s that have been done in the
+     *                      negotiatino (by us and others), first action is the
+     *                      oldest.
+     * @param oppMods       a map of {@link OpponentModel}s, one for each
+     *                      {@link PartyId} encountered in the negotiation
+     * @param reporter      the {@link Reporter} that can be used to log
+     *                      messages. Should equal {@link BoaParty}'s reporter
+     */
+    private BoaState(Settings settings, Profile profile, Progress progr,
                      BiddingStrategy bidstrat, AcceptanceStrategy acceptstrat,
                      Class<? extends OpponentModel> omClass, List<Action> actionHistory,
                      Map<PartyId, OpponentModel> oppMods, Reporter reporter) {
-		this.settings = settings;
-		this.profile = profile;
-		this.progress = progr;
-		this.biddingStrategy = bidstrat;
-		this.acceptanceStrategy = acceptstrat;
-		this.opponentModelClass = omClass;
-		this.actionHistory = actionHistory;
-		this.opponentModels = oppMods;
-		this.reporter = reporter;
-	}
+        this.settings = settings;
+        this.profile = profile;
+        this.progress = progr;
+        this.biddingStrategy = bidstrat;
+        this.acceptanceStrategy = acceptstrat;
+        this.opponentModelClass = omClass;
+        this.actionHistory = actionHistory;
+        this.opponentModels = oppMods;
+        this.reporter = reporter;
+    }
 
-	/**
-	 * This should be the first update call used.
-	 * 
-	 * @param newsettings the negoi {@link Settings}
-	 * @param bidstrat    the {@link BiddingStrategy}
-	 * @param acceptstrat the {@link AcceptanceStrategy}
-	 * @param omClass     the class of the {@link OpponentModel}
-	 * @return initialized BoaState
-	 */
-	public BoaState with(Settings newsettings, BiddingStrategy bidstrat,
-			AcceptanceStrategy acceptstrat,
-			Class<? extends OpponentModel> omClass) {
-		if (settings != null)
-			throw new IllegalStateException("settings already set");
-		if (newsettings == null || omClass == null || bidstrat == null
-				|| acceptstrat == null || reporter == null)
-			throw new NullPointerException(
-					"settings, reporter, bidstrat, acceptstrat, omClass and oModels must be not null");
-		return new BoaState(newsettings, null, newsettings.getProgress(),
-				bidstrat, acceptstrat, omClass, actionHistory,
-				new HashMap<PartyId, OpponentModel>(), reporter);
-	}
+    /**
+     * This should be the first update call used.
+     *
+     * @param newsettings the negoi {@link Settings}
+     * @param bidstrat    the {@link BiddingStrategy}
+     * @param acceptstrat the {@link AcceptanceStrategy}
+     * @param omClass     the class of the {@link OpponentModel}
+     * @return initialized BoaState
+     */
+    public BoaState with(Settings newsettings, BiddingStrategy bidstrat,
+                         AcceptanceStrategy acceptstrat,
+                         Class<? extends OpponentModel> omClass) {
+        if (settings != null)
+            throw new IllegalStateException("settings already set");
+        if (newsettings == null || omClass == null || bidstrat == null
+                || acceptstrat == null || reporter == null)
+            throw new NullPointerException(
+                    "settings, reporter, bidstrat, acceptstrat, omClass and oModels must be not null");
+        return new BoaState(newsettings, null, newsettings.getProgress(),
+                bidstrat, acceptstrat, omClass, actionHistory,
+                new HashMap<PartyId, OpponentModel>(), reporter);
+    }
 
-	public BoaState with(Profile newprofile) {
-		return new BoaState(settings, newprofile, progress, biddingStrategy,
-				acceptanceStrategy, opponentModelClass, actionHistory,
-				opponentModels, reporter);
+    public BoaState with(Profile newprofile) {
+        return new BoaState(settings, newprofile, progress, biddingStrategy,
+                acceptanceStrategy, opponentModelClass, actionHistory,
+                opponentModels, reporter);
 
-	}
+    }
 
-	/**
-	 * 
-	 * @return the settings for this negotiation, includes my {@link PartyId},
-	 *         {@link Parameter}, {@link Profile} etc.
-	 */
-	public Settings getSettings() {
-		return settings;
-	}
+    /**
+     * @return the settings for this negotiation, includes my {@link PartyId},
+     * {@link Parameter}, {@link Profile} etc.
+     */
+    public Settings getSettings() {
+        return settings;
+    }
 
-	/**
-	 * 
-	 * @return the {@link Profile} as fetched from the profiles server, or null
-	 *         if no profile was received yet.
-	 */
-	public Profile getProfile() {
-		return profile;
-	}
+    /**
+     * @return the {@link Profile} as fetched from the profiles server, or null
+     * if no profile was received yet.
+     */
+    public Profile getProfile() {
+        return profile;
+    }
 
-	/**
-	 * 
-	 * @param action the action that was done by some participant (possibly us).
-	 * @return new BoaState that includes action.getBid as last bid (if action
-	 *         contains a bid), and with updated opponentmodel of that
-	 *         participant.
-	 * @throws InstantiationFailedException if one of provided classes can not
-	 *                                      be instantiated.
-	 */
-	public BoaState with(Action action) throws InstantiationFailedException {
-		List<Action> newactions = new ArrayList<Action>(actionHistory);
-		newactions.add(action);
-		Progress newprogress = progress;
-		Map<PartyId, OpponentModel> newmodels = opponentModels;
+    /**
+     * @param action the action that was done by some participant (possibly us).
+     * @return new BoaState that includes action.getBid as last bid (if action
+     * contains a bid), and with updated opponentmodel of that
+     * participant.
+     * @throws InstantiationFailedException if one of provided classes can not
+     *                                      be instantiated.
+     */
+    public BoaState with(Action action) throws InstantiationFailedException {
+        List<Action> newactions = new ArrayList<Action>(actionHistory);
+        newactions.add(action);
+        Progress newprogress = progress;
+        Map<PartyId, OpponentModel> newmodels = opponentModels;
 
-		if (action.getActor().equals(settings.getID())) {
-			// ourside, just advance progress
-			if (progress instanceof ProgressRounds) {
-				// bit hacky, maybe we should wait
-				newprogress = ((ProgressRounds) progress).advance();
-			}
-		} else {
-			// not ourselves, update the OpponentModel
-			newmodels = updateModels(action);
-		}
-		return new BoaState(settings, profile, newprogress, biddingStrategy,
-				acceptanceStrategy, opponentModelClass, newactions, newmodels,
-				reporter);
-	}
+        if (action.getActor().equals(settings.getID())) {
+            // ourside, just advance progress
+            if (progress instanceof ProgressRounds) {
+                // bit hacky, maybe we should wait
+                newprogress = ((ProgressRounds) progress).advance();
+            }
+        } else {
+            // not ourselves, update the OpponentModel
+            newmodels = updateModels(action);
+        }
+        return new BoaState(settings, profile, newprogress, biddingStrategy,
+                acceptanceStrategy, opponentModelClass, newactions, newmodels,
+                reporter);
+    }
 
-	/**
-	 * @return the current {@link Progress}
-	 */
-	public Progress getProgress() {
-		return progress;
-	}
+    /**
+     * @return the current {@link Progress}
+     */
+    public Progress getProgress() {
+        return progress;
+    }
 
-	/**
-	 * @return list of actions done so far, oldest action first
-	 */
-	public List<Action> getActionHistory() {
-		return Collections.unmodifiableList(actionHistory);
-	}
+    /**
+     * @return list of actions done so far, oldest action first
+     */
+    public List<Action> getActionHistory() {
+        return Collections.unmodifiableList(actionHistory);
+    }
 
-	/**
-	 * 
-	 * @param bid the {@link Bid} to check
-	 * @return true iff {@link #acceptanceStrategy} says the bid is acceptable
-	 */
-	public boolean isAcceptable(Bid bid) {
-		return acceptanceStrategy.isAcceptable(bid, this);
-	}
+    /**
+     * @param bid the {@link Bid} to check
+     * @return true iff {@link #acceptanceStrategy} says the bid is acceptable
+     */
+    public boolean isAcceptable(Bid bid) {
+        return acceptanceStrategy.isAcceptable(bid, this);
+    }
 
-	/**
-	 * 
-	 * @return the next recommended action according to the
-	 *         {@link #biddingStrategy}
-	 */
-	public Action getAction() {
-		return biddingStrategy.getAction(this);
-	}
+    /**
+     * @return the next recommended action according to the
+     * {@link #biddingStrategy}
+     */
+    public Action getAction() {
+        return biddingStrategy.getAction(this);
+    }
 
-	/**
-	 * 
-	 * @return the Reporter (same as the BoaParty's reporter)
-	 */
-	public Reporter getReporter() {
-		return reporter;
-	}
+    /**
+     * @return the Reporter (same as the BoaParty's reporter)
+     */
+    public Reporter getReporter() {
+        return reporter;
+    }
 
-	/**
-	 * @param action
-	 * @return the map with the opponentmodels, with the model for the party
-	 *         that did the action updated.
-	 * @throws InstantiationFailedException
-	 */
-	private Map<PartyId, OpponentModel> updateModels(Action action)
-			throws InstantiationFailedException {
-		PartyId actor = action.getActor();
-		Map<PartyId, OpponentModel> updated = extendedOpponentModels(actor);
-		updated.put(actor, updated.get(actor).with(action, progress));
-		return updated;
-	}
+    /**
+     * @param action
+     * @return the map with the opponentmodels, with the model for the party
+     * that did the action updated.
+     * @throws InstantiationFailedException
+     */
+    private Map<PartyId, OpponentModel> updateModels(Action action)
+            throws InstantiationFailedException {
+        PartyId actor = action.getActor();
+        Map<PartyId, OpponentModel> updated = extendedOpponentModels(actor);
+        updated.put(actor, updated.get(actor).with(action, progress));
+        return updated;
+    }
 
-	/**
-	 * Get the current opponent model, extended with newparty if there is no
-	 * model yet for newparty.
-	 * 
-	 * @param newparty a possibly new party
-	 * @return a (possibly new) opponentModels map
-	 * @throws InstantiationException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationFailedException
-	 */
-	private Map<PartyId, OpponentModel> extendedOpponentModels(PartyId newparty)
-			throws InstantiationFailedException {
-		// check if this is new party
-		if (opponentModels.containsKey(newparty))
-			return opponentModels;
+    /**
+     * Get the current opponent model, extended with newparty if there is no
+     * model yet for newparty.
+     *
+     * @param newparty a possibly new party
+     * @return a (possibly new) opponentModels map
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws InstantiationFailedException
+     */
+    private Map<PartyId, OpponentModel> extendedOpponentModels(PartyId newparty)
+            throws InstantiationFailedException {
+        // check if this is new party
+        if (opponentModels.containsKey(newparty))
+            return opponentModels;
 
-		Map<PartyId, OpponentModel> newmodels = new HashMap<>(opponentModels);
-		OpponentModel newmodel;
-		try {
-			newmodel = opponentModelClass.newInstance()
-					.with(profile.getDomain(), profile.getReservationBid());
-		} catch (Exception e) {
-			throw new InstantiationFailedException(
-					"Failed to instantiate " + opponentModelClass, e);
-		}
-		newmodels.put(newparty, newmodel);
-		return newmodels;
-	}
+        Map<PartyId, OpponentModel> newmodels = new HashMap<>(opponentModels);
+        OpponentModel newmodel;
+        try {
+            newmodel = opponentModelClass.newInstance()
+                    .with(profile.getDomain(), profile.getReservationBid());
+        } catch (Exception e) {
+            throw new InstantiationFailedException(
+                    "Failed to instantiate " + opponentModelClass, e);
+        }
+        newmodels.put(newparty, newmodel);
+        return newmodels;
+    }
 
 }
--- src/main/java/collabai/group42/DefaultBoa.java
+++ src/main/java/collabai/group42/DefaultBoa.java
@@ -31,147 +31,146 @@ import java.util.logging.Level;
  */
 public abstract class DefaultBoa extends DefaultParty {
 
-	private BoaState negoState;
-	private ProfileInterface profileint = null;
+    private BoaState negoState;
+    private ProfileInterface profileint = null;
 
-	public DefaultBoa() {
-		super();
-		negoState = initialState();
-	}
+    public DefaultBoa() {
+        super();
+        negoState = initialState();
+    }
 
-	public DefaultBoa(Reporter reporter) {
-		super(reporter); // for debugging
-		negoState = initialState();
-	}
+    public DefaultBoa(Reporter reporter) {
+        super(reporter); // for debugging
+        negoState = initialState();
+    }
 
-	/**
-	 * This only supports SAOP protocol because it is not clear how the
-	 * BiddingStrategy could be made more general without basically becoming a
-	 * full Party.
-	 * 
-	 */
-	@Override
-	public Capabilities getCapabilities() {
-		/*
-		 * At this moment only SAOP is supported by BOA. To support others, the
-		 * components also need to get capabilities. FIXME limiting this to
-		 * linearadditive may be overly restrictive #1871
-		 */
-		return new Capabilities(new HashSet<>(Arrays.asList("SAOP")),
-				Collections.singleton(LinearAdditive.class));
-	}
+    /**
+     * This only supports SAOP protocol because it is not clear how the
+     * BiddingStrategy could be made more general without basically becoming a
+     * full Party.
+     */
+    @Override
+    public Capabilities getCapabilities() {
+        /*
+         * At this moment only SAOP is supported by BOA. To support others, the
+         * components also need to get capabilities. FIXME limiting this to
+         * linearadditive may be overly restrictive #1871
+         */
+        return new Capabilities(new HashSet<>(Arrays.asList("SAOP")),
+                Collections.singleton(LinearAdditive.class));
+    }
 
-	/**
-	 * Impementation of generic BOA behaviour.
-	 */
-	@Override
-	public void notifyChange(Inform info) {
-		try {
-			if (info instanceof Settings) {
-				/*
-				 * this should be first call. We can not yet create the state
-				 * because we need to wait for the profile fetch.
-				 */
-				Settings settings = (Settings) info;
-				Class<? extends OpponentModel> omClass = getOpponentModel(
-						settings);
-				BiddingStrategy bidStrat = getBiddingStrategy(settings);
-				AcceptanceStrategy acceptStrat = getAccceptanceStrategy(
-						settings);
-				negoState = negoState.with(settings, bidStrat, acceptStrat,
-						omClass);
-				this.profileint = ProfileConnectionFactory
-						.create(settings.getProfile().getURI(), getReporter());
-				return;
-			}
+    /**
+     * Impementation of generic BOA behaviour.
+     */
+    @Override
+    public void notifyChange(Inform info) {
+        try {
+            if (info instanceof Settings) {
+                /*
+                 * this should be first call. We can not yet create the state
+                 * because we need to wait for the profile fetch.
+                 */
+                Settings settings = (Settings) info;
+                Class<? extends OpponentModel> omClass = getOpponentModel(
+                        settings);
+                BiddingStrategy bidStrat = getBiddingStrategy(settings);
+                AcceptanceStrategy acceptStrat = getAccceptanceStrategy(
+                        settings);
+                negoState = negoState.with(settings, bidStrat, acceptStrat,
+                        omClass);
+                this.profileint = ProfileConnectionFactory
+                        .create(settings.getProfile().getURI(), getReporter());
+                return;
+            }
 
-			// Beyond this point we should have a profile
-			if (negoState.getProfile() == null) {
-				negoState = negoState.with(this.profileint.getProfile());
-			}
+            // Beyond this point we should have a profile
+            if (negoState.getProfile() == null) {
+                negoState = negoState.with(this.profileint.getProfile());
+            }
 
-			if (info instanceof ActionDone) {
-				negoState = negoState.with(((ActionDone) info).getAction());
-			} else if (info instanceof YourTurn) {
-				getConnection().send(getAction());
-				// we will receive ActionDone and update state then
-			} else if (info instanceof Finished) {
-				getReporter().log(Level.INFO, "Final ourcome:" + info);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to handle info " + info, e);
-		}
+            if (info instanceof ActionDone) {
+                negoState = negoState.with(((ActionDone) info).getAction());
+            } else if (info instanceof YourTurn) {
+                getConnection().send(getAction());
+                // we will receive ActionDone and update state then
+            } else if (info instanceof Finished) {
+                getReporter().log(Level.INFO, "Final ourcome:" + info);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to handle info " + info, e);
+        }
 
-	}
+    }
 
-	/**
-	 * Overridable getter for the acceptance strategy. Override if you want to
-	 * hard code a {@link Group42Party} behaviour
-	 * 
-	 * @param settings the {@link Settings}
-	 * @return the {@link OpponentModel} class to use for modeling opponents
-	 * @throws InstantiationFailedException if the requested opponent model can
-	 *                                      not be loaded
-	 */
-	abstract protected Class<? extends OpponentModel> getOpponentModel(
-			Settings settings) throws InstantiationFailedException;
+    /**
+     * Overridable getter for the acceptance strategy. Override if you want to
+     * hard code a {@link Group42Party} behaviour
+     *
+     * @param settings the {@link Settings}
+     * @return the {@link OpponentModel} class to use for modeling opponents
+     * @throws InstantiationFailedException if the requested opponent model can
+     *                                      not be loaded
+     */
+    abstract protected Class<? extends OpponentModel> getOpponentModel(
+            Settings settings) throws InstantiationFailedException;
 
-	/**
-	 * Overridable getter for the acceptance strategy. Override if you want to
-	 * hard code a {@link Group42Party} behaviour
-	 * 
-	 * @param settings the {@link Settings}
-	 * @return the {@link BiddingStrategy} to use for determining the next
-	 *         {@link Action}
-	 * @throws InstantiationFailedException if the requested opponent model can
-	 *                                      not be loaded
-	 */
-	abstract protected BiddingStrategy getBiddingStrategy(Settings settings)
-			throws InstantiationFailedException;
+    /**
+     * Overridable getter for the acceptance strategy. Override if you want to
+     * hard code a {@link Group42Party} behaviour
+     *
+     * @param settings the {@link Settings}
+     * @return the {@link BiddingStrategy} to use for determining the next
+     * {@link Action}
+     * @throws InstantiationFailedException if the requested opponent model can
+     *                                      not be loaded
+     */
+    abstract protected BiddingStrategy getBiddingStrategy(Settings settings)
+            throws InstantiationFailedException;
 
-	/**
-	 * Overridable getter for the acceptance strategy. Override if you want to
-	 * hard code a {@link BoaState} behaviour
-	 * 
-	 * @param settings the {@link Settings}
-	 * @return the {@link AcceptanceStrategy} to use for determining if a bid is
-	 *         acceptable
-	 * @throws InstantiationFailedException if the requested opponent model can
-	 *                                      not be loaded
-	 */
-	abstract protected AcceptanceStrategy getAccceptanceStrategy(
-			Settings settings) throws InstantiationFailedException;
+    /**
+     * Overridable getter for the acceptance strategy. Override if you want to
+     * hard code a {@link BoaState} behaviour
+     *
+     * @param settings the {@link Settings}
+     * @return the {@link AcceptanceStrategy} to use for determining if a bid is
+     * acceptable
+     * @throws InstantiationFailedException if the requested opponent model can
+     *                                      not be loaded
+     */
+    abstract protected AcceptanceStrategy getAccceptanceStrategy(
+            Settings settings) throws InstantiationFailedException;
 
-	/**
-	 * Factory method to create initial state. Override to insert mock
-	 * 
-	 * @return initial BoaState
-	 */
-	protected BoaState initialState() {
-		return new BoaState(getReporter());
-	}
+    /**
+     * Factory method to create initial state. Override to insert mock
+     *
+     * @return initial BoaState
+     */
+    protected BoaState initialState() {
+        return new BoaState(getReporter());
+    }
 
-	/**
-	 * @return the most recent bid that was offered, or null if no offer has
-	 *         been done yet.
-	 */
-	protected Bid getLastBid() {
-		List<Action> actions = negoState.getActionHistory();
-		for (int n = actions.size() - 1; n >= 0; n--) {
-			Action action = actions.get(n);
-			if (action instanceof Offer) {
-				return ((Offer) action).getBid();
-			}
-		}
-		return null;
-	}
+    /**
+     * @return the most recent bid that was offered, or null if no offer has
+     * been done yet.
+     */
+    protected Bid getLastBid() {
+        List<Action> actions = negoState.getActionHistory();
+        for (int n = actions.size() - 1; n >= 0; n--) {
+            Action action = actions.get(n);
+            if (action instanceof Offer) {
+                return ((Offer) action).getBid();
+            }
+        }
+        return null;
+    }
 
-	private Action getAction() {
-		Bid lastBid = getLastBid();
-		if (lastBid != null && negoState.isAcceptable(lastBid)) {
-			return new Accept(negoState.getSettings().getID(), lastBid);
-		}
-		return negoState.getAction();
-	}
+    private Action getAction() {
+        Bid lastBid = getLastBid();
+        if (lastBid != null && negoState.isAcceptable(lastBid)) {
+            return new Accept(negoState.getSettings().getID(), lastBid);
+        }
+        return negoState.getAction();
+    }
 
 }
